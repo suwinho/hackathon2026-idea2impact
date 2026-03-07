4bcf65daf17b4595737c3c83cb07198120c2c8fb
smietanka4 <wloczewskikarol5@gmail.com>
2026-03-07 21:12:01 +0100

    fronted fixy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [
     react({
