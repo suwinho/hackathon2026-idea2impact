6538f728de1dd8140a263906708adaa3d8edda1a
smietanka4 <wloczewskikarol5@gmail.com>
2026-03-07 20:39:57 +0100

    pierwsze pop-upy z formularzem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   plugins: [
     react({
       babel: {
