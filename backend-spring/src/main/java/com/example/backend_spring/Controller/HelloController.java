1c13a9820986d13196f97fedfdc95d7d54aebb2b
suwinhoo <mateuszsuwinski@proton.me>
2026-03-07 19:37:47 +0100

    backend hello world

---
+++ backend-spring/src/main/java/com/example/backend_spring/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.backend_spring.Controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public Map<String, String> hello() {
+        return Map.of(
+            "status", "Działa!",
+            "wiadomosc", "Witaj w systemie wsparcia dla NGO",
+            "zespol", "Idea2Impact Team"
+        );
+    }
+}
