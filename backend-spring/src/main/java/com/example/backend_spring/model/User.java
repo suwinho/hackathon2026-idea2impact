2082be87d5c9247ca256f718777c788c186512a8
smietanka4 <wloczewskikarol5@gmail.com>
2026-03-07 22:55:35 +0100

    naprawy komunikacji backendu i frontendu

--- backend-spring/src/main/java/com/example/backend_spring/model/User.java
+++ backend-spring/src/main/java/com/example/backend_spring/model/User.java
@@ -8,12 +8,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class User {
     @Id
