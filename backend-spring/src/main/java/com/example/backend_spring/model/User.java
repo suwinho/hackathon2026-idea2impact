d9ce2d84ef7a61577205fd32a61778dd3907d667
suwinhoo <mateuszsuwinski@proton.me>
2026-03-07 20:55:41 +0100

    forms controlls

---
+++ backend-spring/src/main/java/com/example/backend_spring/model/User.java
@@ -0,0 +1,32 @@
+package com.example.backend_spring.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_data_id", referencedColumnName = "id")
+    private UserData userData;
+}
